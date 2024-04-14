8d7a11404f44b0b970d31ff78ddaeff996d8cf50
秦超 <115026744+kkkh@users.noreply.github.com>
2024-04-14 10:42:19 +0800

    Update config.ts
    网站基本信息 社交链接更新

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,19 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://43322.cn/", // replace this with your deployed domain
+  author: "秦超",
+  desc: "学有所成",
+  title: "学有所成",
+  ogImage: "qinchao-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  lang: "zh", // html lang code. Set this empty and default will be "en"
+  langTag: ["zh-CN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
@@ -26,122 +26,122 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: ` ${SITE.title} on Github`,
-    active: true,
-  },
-  {
-    name: "Facebook",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Facebook`,
-    active: true,
-  },
-  {
-    name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Instagram`,
-    active: true,
-  },
-  {
-    name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on LinkedIn`,
-    active: true,
-  },
-  {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
+    href: "https://github.com/kkkh",
+    linkTitle: ` ${SITE.title} 我的 Github`,
     active: false,
   },
   {
+    name: "Facebook",
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 Facebook`,
+    active: false,
+  },
+  {
+    name: "Instagram",
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 Instagram`,
+    active: false,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 LinkedIn`,
+    active: false,
+  },
+  {
+    name: "Mail",
+    href: "mailto:qinchao@43322.cn",
+    linkTitle: `发送电子邮件至 ${SITE.title}`,
+    active: true,
+  },
+  {
     name: "Twitter",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Twitter`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 Twitter`,
     active: false,
   },
   {
     name: "Twitch",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Twitch`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 Twitch`,
     active: false,
   },
   {
     name: "YouTube",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on YouTube`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 YouTube`,
     active: false,
   },
   {
     name: "WhatsApp",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on WhatsApp`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 WhatsApp`,
     active: false,
   },
   {
     name: "Snapchat",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Snapchat`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 Snapchat`,
     active: false,
   },
   {
     name: "Pinterest",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Pinterest`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 Pinterest`,
     active: false,
   },
   {
     name: "TikTok",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on TikTok`,
-    active: false,
+    href: "https://www.douyin.com/user/MS4wLjABAAAA0hh227P5Ylh12-y2pIocvYK0GIDwpVr4IjPj4FhZtGk",
+    linkTitle: `${SITE.title} 我的 抖音`,
+    active: true,
   },
   {
     name: "CodePen",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on CodePen`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 CodePen`,
     active: false,
   },
   {
     name: "Discord",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Discord`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 Discord`,
     active: false,
   },
   {
     name: "GitLab",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on GitLab`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 GitLab`,
     active: false,
   },
   {
     name: "Reddit",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Reddit`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 Reddit`,
     active: false,
   },
   {
     name: "Skype",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Skype`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 Skype`,
     active: false,
   },
   {
     name: "Steam",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://steamcommunity.com/id/uukh/",
     linkTitle: `${SITE.title} on Steam`,
     active: false,
   },
   {
     name: "Telegram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Telegram`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 Telegram`,
     active: false,
   },
   {
     name: "Mastodon",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Mastodon`,
+    href: "https://github.com/kkkh",
+    linkTitle: `${SITE.title} 我的 Mastodon`,
     active: false,
   },
 ];
